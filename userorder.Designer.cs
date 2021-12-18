fa739e9f1d639c72b213f97a834087d6d9816172
glytchybg <glitchybg@gmail.com>
2021-12-18 12:48:33 +0200

    I'm a lil bit confused

--- userorder.Designer.cs
+++ userorder.Designer.cs
@@ -28,25 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.ordernewBurgerMenu = new System.Windows.Forms.DataGridView();
             this.orderbtn = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.Quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.ordernewBurgerMenu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(12, 196);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown1.TabIndex = 0;
             // 
             // ordernewBurgerMenu
             // 
+            this.ordernewBurgerMenu.AllowUserToAddRows = false;
+            this.ordernewBurgerMenu.AllowUserToDeleteRows = false;
             this.ordernewBurgerMenu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.ordernewBurgerMenu.Location = new System.Drawing.Point(12, 12);
             this.ordernewBurgerMenu.Name = "ordernewBurgerMenu";
+            this.ordernewBurgerMenu.ReadOnly = true;
             this.ordernewBurgerMenu.Size = new System.Drawing.Size(480, 178);
             this.ordernewBurgerMenu.TabIndex = 1;
             // 
@@ -60,27 +60,67 @@
             this.orderbtn.UseVisualStyleBackColor = true;
             this.orderbtn.Click += new System.EventHandler(this.orderbtn_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Quantity});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 288);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(524, 260);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // Quantity
+            // 
+            this.Quantity.HeaderText = "quantity";
+            this.Quantity.MaxInputLength = 2;
+            this.Quantity.Name = "Quantity";
+            this.Quantity.Width = 50;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(259, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // userorder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.orderbtn);
             this.Controls.Add(this.ordernewBurgerMenu);
-            this.Controls.Add(this.numericUpDown1);
             this.Name = "userorder";
             this.Text = "userorder";
             this.Load += new System.EventHandler(this.userorder_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ordernewBurgerMenu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.DataGridView ordernewBurgerMenu;
         private System.Windows.Forms.Button orderbtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quantity;
+        private System.Windows.Forms.Button button1;
     }
 }
--- userorder.cs
+++ userorder.cs
@@ -22,22 +22,41 @@ namespace BurgeriVisual
         private void userorder_Load(object sender, EventArgs e)
         {
             GetMenu();
+            GetUserQuantity();
+
         }
         private void GetMenu()
         {
             DataSet ds = new DataSet();
-
-            SqlCommand com = new SqlCommand("SELECT * FROM burgers.burgerTypes", sqlcon);
             sqlcon.Open();
             using (sqlcon)
             {
-                using (com)
-                {
+
                     SqlDataAdapter dataadapter = new SqlDataAdapter("SELECT * FROM burgers.burgerTypes", sqlcon);
                     dataadapter.Fill(ds, "burgerName");
                     ordernewBurgerMenu.DataSource = ds;
                     ordernewBurgerMenu.DataMember = "burgerName";
                     this.ordernewBurgerMenu.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            }
+        }
+        List<int> burgQuantity = new List<int>();
+        private void GetUserQuantity()
+        {
+            SqlConnection sconn = new SqlConnection("server=DESKTOP-JKT9IB6;database=burgers;Integrated Security=true;");
+            sconn.Open();
+            DataSet ds = new DataSet();
+            SqlDataAdapter dataadapter = new SqlDataAdapter("SELECT * FROM burgers.burgerTypes", sconn);
+            sconn.Close();
+            dataadapter.Fill(ds, "burgerTypes");
+            int rowcount = Convert.ToInt32(dataGridView1.Rows.Count);
+            this.dataGridView1.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            int indexRow = dataGridView1.RowCount;
+            if (indexRow >= 0)
+            {
+                for (int i = 1; i <= rowcount; i++)
+                {
+                    burgQuantity.Add(Convert.ToInt16(dataGridView1.SelectedCells[i].Value));
+
                 }
             }
         }
@@ -56,14 +75,13 @@ namespace BurgeriVisual
                     priceList.Add(Convert.ToDecimal(reader["price"]));
                 }
             }
-            for (int i = 0; i < priceList.Count; i++)
+            for (int i = 0; i < burgQuantity.Count; i++)
             {
-                price += priceList[i] * numericUpDown1.Value;
+                price += priceList[i] * Convert.ToDecimal(burgQuantity[i]);
             }
             MessageBox.Show("Cenata e: " + price.ToString());
             return price;
         }
-
         private void orderbtn_Click(object sender, EventArgs e)
         {
             CalculateOrderPrice();
